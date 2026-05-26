e0540b1beb427e4f7a28b05a33deb03f1eec0ec1
SjoukjevanWieren <s.van.wieren@kpnmail.nl>
2026-05-26 14:35:39 +0200

    improve docstrings

--- inflammation/models.py
+++ inflammation/models.py
@@ -18,17 +18,32 @@ def load_csv(filename):
     return np.loadtxt(fname=filename, delimiter=',')
 
 
-def daily_mean(data):
-    """Calculate the daily mean of a 2d inflammation data array."""
+def daily_mean(data:np.array) -> np.array:
+    """ Calculate the daily mean of a 2d inflammation data array
+
+    :param data: data
+    :return: daily mean
+    """
+    #"""Calculate the daily mean of a 2d inflammation data array."""
     return np.mean(data, axis=0)
 
 
-def daily_max(data):
-    """Calculate the daily max of a 2d inflammation data array."""
+def daily_max(data:np.array) -> np.array:
+    """Calculate the daily max of a 2d inflammation data array.
+
+    :param data: data
+    :return: daily max
+    """    
+    #"""Calculate the daily max of a 2d inflammation data array."""
     return np.max(data, axis=0)
 
 
-def daily_min(data):
-    """Calculate the daily min of a 2d inflammation data array."""
+def daily_min(data:np.array) -> np.array: #example of an hint (useful for documentation)
+    """Calculate the daily min of a 2d inflammation data array.
+
+    :param data: data
+    :return: daily min
+    """    
+    #"""Calculate the daily min of a 2d inflammation data array."""
     return np.min(data, axis=0)
 
