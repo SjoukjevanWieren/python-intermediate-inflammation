b54266a441f896afdf2c4dbe7a0489c963ef6f73
SjoukjevanWieren <s.van.wieren@kpnmail.nl>
2026-05-27 13:54:28 +0200

    fixed arguments .to pass data THANKS TO CATA

--- inflammation/analysis.py
+++ inflammation/analysis.py
@@ -122,4 +122,5 @@ if __name__ == '__main__':
     args = parser.parse_args()
     
     # Run the function using the provided argument
-    analyse_data(args.data_dir)
+    datasource = CSVDataSource(args.data_dir)
+    analyse_data(datasource)
